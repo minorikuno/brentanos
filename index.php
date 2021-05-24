e5711898b3c7f6f4ff98c38ecb300259ae296c8e
MinoriKUNO <minori.fr@gmail.com>
2021-05-24 18:04:40 +0200

    24 may 2021

---
+++ index.php
@@ -0,0 +1,43 @@
+<?php
+
+
+
+?>
+
+<!DOCTYPE html>
+<html lang="en">
+
+<head>
+  <meta charset="UTF-8">
+  <meta http-equiv="X-UA-Compatible" content="IE=edge">
+  <meta name="viewport" content="width=device-width, initial-scale=1.0">
+  <meta name="description" content="Librairie dans Avenue de l'Opéra vente des livres en anglais, cartes postales, cartes d'anniversaire, décorations maison, affiches et cadres | Paris 2ème">
+  <title>Librairie américaine & papeterie & décoration | Brentano's</title>
+  <link rel="icon" type="imagepng" href="public/img/logo_b_4.png">
+  <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.0.1/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-+0n0xVW2eSR5OomGNYDnhzAbDsOXxcvSN1TPprVMTNDbiYZCxYbOOl7+AMvyTG2x" crossorigin="anonymous">
+  <link rel="stylesheet" href="public/css/style.css">
+</head>
+
+<body>
+
+  <header>
+    <div class="logo">
+      <img src="public/img/logo_brentanos.png" alt="Logo de Brentanos">
+    </div>
+    <nav>
+      <ul>
+        <li><a href="#">Histoire</a></li>
+        <li><a href="#">à propos de nous</a></li>
+        <li><a href="#">Article</a></li>
+        <li><a href="#">Boutique</a></li>
+        <li><a href="contact.php">Contact</a></li>
+      </ul>
+    </nav>
+  </header>
+
+
+  <script src="https://cdn.jsdelivr.net/npm/@popperjs/core@2.9.2/dist/umd/popper.min.js" integrity="sha384-IQsoLXl5PILFhosVNubq5LC7Qb9DXgDA9i+tQ8Zj3iwWAwPtgFTxbJ8NT4GN1R8p" crossorigin="anonymous"></script>
+  <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.0.1/dist/js/bootstrap.min.js" integrity="sha384-Atwg2Pkwv9vp0ygtn1JAojH0nYbwNJLPhwyoVbhoPwBhjQPR5VtM2+xf0Uwh9KtT" crossorigin="anonymous"></script>
+</body>
+
+</html>
